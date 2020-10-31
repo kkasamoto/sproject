24b4f4949d7bb67417fd588d6474669241553cef
dokugakun <dokugakun.k789@outlook.jp>
2020-10-31 19:31:07 +0900

    mod base for accounts

--- sproject/settings/base.py
+++ sproject/settings/base.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -79,6 +80,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'accounts.User'
 
 
 # Internationalization
